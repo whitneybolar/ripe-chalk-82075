6d1ca6010a3ae83e471c00640fb17f927470454a
Whitney Bolar <wbolar@eagles.ewu.edu>
2019-06-05 14:08:07 -0700

    go live!!

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,20 @@ module.exports = {
                 pageContextProperty: `menus`,
                 menus: require('./src/data/menus.json'),
             }
-        }
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+              name: "Phantom",
+              short_name: "Phantom",
+              start_url: "/",
+              background_color: "#6b37bf",
+              theme_color: "#6b37bf",
+              // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+              // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+              display: "standalone",
+              icon: "./src/icon.png", // This path is relative to the root of the site.
+            },
+        },
     ]
 };
